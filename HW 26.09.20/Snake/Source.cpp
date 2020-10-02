762b8ae35ceed7e6270629163c1f103ae8e478ec
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2020-10-02 21:10:30 +0300

    added ex 1 (2d-arrays)

--- HW 26.09.20/Snake/Source.cpp
+++ HW 26.09.20/Snake/Source.cpp
@@ -2,23 +2,44 @@
 
 int main()
 {
-	std::cout << "Enter the direction of the snake(right or down): ";
-	bool
-	/*const int Rows = 4;
-	const int Cols = 6;
+	bool flag;
+	std::cout << "Enter the direction (left or down): "; 
+	char dir[5];
+	std::cin >> dir;
+	if (dir[0] == 'd')
+		flag = true;
+	else
+		flag = false;
+
+	const int Rows = 3;
+	const int Cols = 4;
 
 	int snake[Rows][Cols];
 	int digit = 1;
-	for (size_t rows = 0; rows < Rows; ++rows)
+
+	if (flag)
 	{
-		if (rows % 2 == 0)
-			for (size_t cols = 0; cols < Cols; ++cols)
-				snake[rows][cols] = digit++;
-
-		else
-			for (int cols = Cols - 1; cols >= 0; --cols)
-				snake[rows][cols] = digit++;
-
+		for (size_t cols = 0; cols < Cols; ++cols)
+		{
+			if (cols % 2 == 0)
+				for (size_t rows = 0; rows < Rows; ++rows)
+					snake[rows][cols] = digit++;
+			else
+				for (int rows = Rows - 1; rows >= 0; --rows)
+					snake[rows][cols] = digit++;
+		}
+	}
+	else
+	{
+		for (size_t rows = 0; rows < Rows; ++rows)
+		{
+			if (rows % 2 == 0)
+				for (size_t cols = 0; cols < Cols; ++cols)
+					snake[rows][cols] = digit++;
+			else
+				for (int cols = Cols - 1; cols >= 0; --cols)
+					snake[rows][cols] = digit++;
+		}
 	}
 
 	for (size_t rows = 0; rows < Rows; ++rows)
@@ -27,7 +48,7 @@ int main()
 			std::cout << snake[rows][cols] << ' ';
 
 		std::cout << '\n';
-	}*/
+	}
 
 	system("pause");
 	return(0);
