9e7a4fd89cc1de8b17e346c148a847c8392d59cf
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2020-11-13 20:29:01 +0300

    added task about "...=..."

--- Functions_HW/Find value/Find value.cpp
+++ Functions_HW/Find value/Find value.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 
 
-bool find_value(const char** vars, const char* name, char* value) {
-    size_t i = 0;
-    while (vars[i++]) {
+bool find_value(const char** vars, const char* name, char* value, int size ) {
+    int i = 0;
+    while (i<size) {
         for (size_t j = 0; j < 12; ++j) {                
                 if (vars[i][j] == '=') {
                     j++;
-                    for (size_t k = 0; k < (12 - j); ++k) 
+                    size_t k = 0;
+                    while (vars[i][j] != '\0') {
                         value[k] = vars[i][j++];
+                        ++k;
+                    }
+                    value[k] = '\0';
                     return true;
                 }
                 if (name[j] != vars[i][j])
                     break;
             }
-        return false;
+        ++i;
     }
+    return false;
  }
 
 int main() {
     
-   const char vars[3][12] = { "abc=cde", "user=cap", "live=evil" };
+   const char* const vars[] = { "abc=cde", "user=cap", "live=evil" };
    char value[12];
 
-    std::cout<<find_value((const char**)vars, "user", (char*) value)<<'\n';//-> true, value = "cap"
-    //find_value(&vars, "system", &value); //-> false
+   if (find_value((const char**)vars, "live", (char*)value, sizeof(vars)/4))
+       std::cout << value << '\n';
+   else
+       std::cout << "No\n";
 
     system("pause");
     return 0;
--- Recursion_HW/Palindrome/Palindrome.cpp
+++ Recursion_HW/Palindrome/Palindrome.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
-bool check_palindrome(char* str, int len,int i) {
+/*bool check_palindrome(char* str, int len,int i) {
     if(len-2*i<1)
         return true;
     if (str[i] != str[len - 1-i]) 
         return false;
     check_palindrome(str, len,++i);
+}*/
+
+bool check_palindrome(char* str, int len) {
+    if (len <= 0)
+        return true;
+    if (str[0] != str[len - 1])
+        return false;
+    check_palindrome(str + 1, len - 2);
 }
 
 int main()
 {
     char str[] = "abcdcba";
 
-    std::cout << check_palindrome(str, 7, 0) << '\n';
+    std::cout << check_palindrome(str, 7) << '\n';
 
     system("pause");
     return 0;
