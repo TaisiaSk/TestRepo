1376c2e0c3d961febbb739f098e171c7e18e5df5
TaisiaSk <71456151+TaisiaSk@users.noreply.github.com>
2020-11-21 01:54:18 +0300

    added bmplmg

---
+++ BmpImg/ImgFunctions.cpp
@@ -0,0 +1,159 @@
+#include "ImgFunctions.h"
+#include <fstream>
+#include <exception>
+#include <iostream>
+
+int get_offset(int width)
+{
+    int offset = 0;
+    if (width % 4)
+        offset = 4 - (3 * width) % 4;
+    return offset;
+}
+
+RgbImg read_rgb_img(const char filename[])
+{
+    std::ifstream bmp_in(filename, std::ios::binary);
+    if (!bmp_in.is_open())
+        throw std::runtime_error("Failed to open input file");
+
+    BITMAPFILEHEADER bmfh;
+    BITMAPINFOHEADER bmih;
+
+    bmp_in.read((char*)&bmfh, sizeof(BITMAPFILEHEADER));
+    bmp_in.read((char*)&bmih, sizeof(BITMAPINFOHEADER));
+
+    const int offset = get_offset(bmih.biWidth);
+
+    RgbImg img;
+    img.height = bmih.biHeight;
+    img.width = bmih.biWidth;
+    img.pixels = new RGB * [img.height];
+
+    for (size_t row = 0; row < img.height; ++row)
+    {
+        img.pixels[row] = new RGB[img.width];
+        for (size_t col = 0; col < img.width; ++col)
+            bmp_in.read((char*)&img.pixels[row][col], 3);
+        bmp_in.seekg(offset, std::ios::cur);
+    }
+
+    bmp_in.close();
+    return img;
+}
+
+void write_rgb_img(char const filename[], RgbImg const& img)
+{
+    std::ofstream bmp_out(filename, std::ios::binary);
+    if (!bmp_out.is_open())
+        throw std::runtime_error("Failed to open output file");
+
+    const int offset = get_offset(img.width);
+
+    BITMAPFILEHEADER bmfh;
+    char bfType[] = { 'B', 'M' };
+    bmfh.bfType = *((WORD*)bfType); // ('M' << 8) | 'B';
+    bmfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+    bmfh.bfSize = bmfh.bfOffBits + img.height * img.width * 3;
+    bmfh.bfReserved1 = bmfh.bfReserved2 = 0;
+
+    BITMAPINFOHEADER bmih;
+    bmih.biClrImportant = bmih.biClrUsed = bmih.biCompression = 0;
+    bmih.biPlanes = bmih.biXPelsPerMeter = bmih.biYPelsPerMeter = 1;
+    bmih.biSize = sizeof(BITMAPINFOHEADER);
+    bmih.biSizeImage = bmfh.bfSize - bmfh.bfOffBits;
+    bmih.biHeight = img.height;
+    bmih.biWidth = img.width;
+    bmih.biBitCount = 24;
+
+    bmp_out.write((char*)&bmfh, sizeof(BITMAPFILEHEADER));
+    bmp_out.write((char*)&bmih, sizeof(BITMAPINFOHEADER));
+
+    BYTE* offset_array = new BYTE[offset];
+    for (int i = 0; i < offset; ++i)
+        offset_array[i] = 0;
+
+    for (size_t row = 0; row < img.height; ++row)
+    {
+        for (size_t col = 0; col < img.width; ++col)
+            bmp_out.write((char*)&img.pixels[row][col], 3);
+        bmp_out.write((char*)offset_array, offset);
+    }
+
+    delete[] offset_array;
+    bmp_out.close();
+}
+
+void print_rgb_img_info(const char filename[])
+{
+    std::ifstream bmp_in(filename, std::ios::binary);
+    if (!bmp_in.is_open())
+        throw std::runtime_error("Failed to open input file");
+
+    BITMAPFILEHEADER bmfh;
+    BITMAPINFOHEADER bmih;
+
+    bmp_in.read((char*)&bmfh, sizeof(BITMAPFILEHEADER));
+    bmp_in.read((char*)&bmih, sizeof(BITMAPINFOHEADER));
+
+    std::cout << bmfh.bfSize << std::endl;
+    std::cout << bmfh.bfOffBits << std::endl;
+
+    std::cout << bmih.biSize << std::endl;
+    std::cout << bmih.biHeight << std::endl;
+    std::cout << bmih.biWidth << std::endl;
+    std::cout << bmih.biSizeImage << std::endl;
+
+    bmp_in.close();
+}
+
+void black_white(RgbImg const& img)
+{
+    BYTE sum;
+    for (int i = 0; i < img.height; ++i)
+        for (int j = 0; j < img.width; ++j) {
+            sum = (img.pixels[i][j].Blue + img.pixels[i][j].Green + img.pixels[i][j].Red)/3;
+            img.pixels[i][j] = { sum,sum,sum };
+        }
+}
+
+RgbImg blur(RgbImg const& img, const int arg)
+{
+    RgbImg img_dop;
+    img_dop.height = img.height;
+    img_dop.width = img.width;
+    img_dop.pixels = new RGB * [img.height];
+    for (size_t row = 0; row < img_dop.height; ++row)
+        img_dop.pixels[row] = new RGB[img_dop.width];
+
+    BYTE sum_r=0;
+    BYTE sum_g = 0;
+    BYTE sum_b=0;
+    int count = 0;
+    for(size_t i=arg;i<img.height;i++)
+        for (size_t j = arg; j < img.width; ++j)
+        {
+            for (size_t k = i - arg; k <= i + arg; k++)
+            {
+                if (k == img.height)
+                    break;
+                for (size_t l = j - arg; l <= i + arg; l++)
+                {
+                    if (l == img.width)
+                        break;
+                    if ((k == i) && (l == j))
+                        continue;
+                    sum_r += img.pixels[k][l].Red;
+                    sum_g += img.pixels[k][l].Green;
+                    sum_b += img.pixels[k][l].Blue;
+                    ++count;
+                }
+            }
+            img_dop.pixels[i][j] = { (BYTE)(sum_r / count),(BYTE)(sum_g / count),(BYTE)(sum_b / count) };
+            sum_r = 0;
+            sum_g = 0;
+            sum_b = 0;
+            count = 0;
+        }
+    return img_dop;
+}
---
+++ BmpImg/ImgFunctions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "BmpStructures.h"
+
+RgbImg read_rgb_img(char const filename[]);
+void write_rgb_img(char const filename[], RgbImg const& img);
+void print_rgb_img_info(char const filename[]);
+void black_white(RgbImg const& img);
+RgbImg blur(RgbImg const& img, const int arg);
